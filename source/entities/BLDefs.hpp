25e1bd5470a53bd0f6fd11478b4553488fd96eab
tessellates <tobias.e.huysentruyt@gmail.com>
2024-03-06 21:25:49 +0100

    prep for float

--- source/entities/BLDefs.hpp
+++ source/entities/BLDefs.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
 #include "imgui.h"
+#include <SDL.h>
 
 using Vec2 = ImVec2;
+using Rect = ImVec4;
--- source/entities/TextureInfo.hpp
+++ source/entities/TextureInfo.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SDL.h>
+#include "BLDefs.hpp"
 
 struct TextureInfo
 {
