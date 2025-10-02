b80dff65de061d8a08419269635c2ad254f70a3b
Tobias Huysentruyt <butanix@mac.home>
2025-10-02 16:55:13 +0200

    clean up -- amend

--- build.py
+++ build.py
@@ -1,13 +1,59 @@
-import xcpp_build_tools.xcpp_build_tools.cppxbuild as xcbt
+#!/usr/bin/python3
+from scripts import script_utils
+import os
+import platform
 import argparse
+import subprocess
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Build system for C++ projects using CMake.")
-    parser.add_argument('commands', nargs='*', help="Commands, targets to build, or actions to apply. Use 'all' to build all targets.")
-    return parser.parse_args()
+build_type = 'Release'
 
+def main(setup = True, emscripten=False, runonly=True):
+    venv_context = script_utils.VenvContext("blink_lite_venv")
+    os.environ['REPO_DIR'] = os.getcwd()
+    
+    if not runonly:
+        if setup:
+            script_utils.create_venv(venv_context)
+        if setup:
+            script_utils.initialize_venv(venv_context, os.path.join('data', 'requirements.txt'))
+
+        profile = 'default'
+        hostprofile = profile
+        if emscripten:
+            profile_path = os.path.join('data', 'emprofile')
+            if not os.path.exists(profile_path):
+                script_utils.generate_emscripten_profile(profile_path)
+            hostprofile = profile_path
+
+        if setup:
+            try:
+                venv_context.run_command('conan', 'profile', 'detect')
+            except: 
+                pass
+            
+            command_args = ['install', '.', '-of=build', '-s', f'build_type={build_type}', '--build=missing', f"-pr:b={profile}", f"-pr:h={hostprofile}"]
+            if emscripten:
+                command_args += ['-o', 'emscripten=True']
+            venv_context.run_command('conan', *command_args)
+
+        venv_context.run_command('conan', 'build', '.',  '-of=build', '-s', f'build_type={build_type}', f"-pr:b={profile}", f"-pr:h={hostprofile}")
+        subprocess.check_call([f'./build/bin/driver'])
+    else:
+        if platform.system() == 'Windows':
+            subprocess.check_call([os.path.join('install', 'run', 'driver.exe')])
+        else:
+            subprocess.check_call([f'./build/bin/driver'])
+        
+    print('-- end build script --')
+    return
+    
 if __name__ == "__main__":
-    args = parse_arguments()
-    xbuild = xcbt.XBuild()
-    xbuild.handle_arguments(args.commands)
-
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('-ns', '--no-setup', action='store_true', help='no setup')
+    parser.add_argument('-em', '--emscripten', action='store_true', help='enable emscripten -- compile wasm, js and html file instead of binary')
+    parser.add_argument('-ro', '--run-only', action='store_true', help='run only -- launches your application')
+    parser.add_argument('-cb', '--clean-build', action='store_true', help='clean build')
+    argsparsed = parser.parse_args()
+    if argsparsed.clean_build:
+        script_utils.remove_folder('build')
+    main(not argsparsed.no_setup, argsparsed.emscripten,  argsparsed.run_only)
---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps
+import os
+
+class BlinkLite1(ConanFile):
+    name = "blinklite1"
+    version = "1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"emscripten": [True, False]}
+    default_options = {"emscripten": False}
+
+    def requirements(self):
+        self.requires("sdl/3.2.6", options={"shared": False})
+        self.requires("glm/1.0.1")
+        if self.options.emscripten:
+            self.tool_requires("emsdk/3.1.73")
+        self.tool_requires("cmake/[>=3.25]")
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        install_dir = os.environ['REPO_DIR']
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        self.run(f"cmake --install . --prefix={install_dir} --config {self.settings.build_type}")
